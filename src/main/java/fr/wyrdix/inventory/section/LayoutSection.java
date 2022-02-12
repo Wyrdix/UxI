11baa8dc1050c0bc4a128cb453579a0266202069
Wyrdix <nathancoquerel@gmail.com>
2022-02-12 22:59:00 +0100

    Add LayoutSection

---
+++ src/main/java/fr/wyrdix/inventory/section/LayoutSection.java
@@ -0,0 +1,38 @@
+package fr.wyrdix.inventory.section;
+
+import fr.wyrdix.inventory.GuiPosition;
+import org.apache.commons.lang.Validate;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+public class LayoutSection extends SimpleGuiSection {
+
+    public LayoutSection(@NonNull GuiSection parent, String[] layout, char match) {
+        super(parent);
+        Validate.notNull(parent);
+        Validate.notNull(layout);
+
+        int height = layout.length;
+        Validate.isTrue(height > 0);
+
+        int size = parent.getFields().size();
+        for (int i = 0; i < size; i++) {
+            GuiPosition position = parent.getFields().get(i);
+
+            char c = layout[position.getY()].charAt(position.getX());
+            if (c != match) continue;
+            parentFields.add(position);
+            fields.add(new GuiPosition(this, fields.size(), position.getX(), position.getY()));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "LayoutSection{" +
+                "fields=" + fields +
+                ", parentFields=" + parentFields +
+                ", sections=" + getSubSections() +
+                ", components=" + getComponents() +
+                ", parent=" + getParent() +
+                '}';
+    }
+}
